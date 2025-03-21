d2f6b4ebd33bb9ecf00d31841a7b8649088dd269
Chris9575 <chris.tseng0705@gmail.com>
2025-03-21 10:07:07 +0800

    Delete week03 directory

--- week03/week03-1-2.c
+++
@@ -1,9 +0,0 @@
-//week03-1.cpp 二合一的c++版本
-//Leetcode 2579. Count Total Number of Colored Cells
-class Solution {
-public:
-    long long coloredCells(int n) {
-        long long ans=n;//要多寫這行，把數字升級，變64位元(很長很長的整數)
-        return ans*ans +(ans-1)*(ans-1);//c/c++/c#都要加分號
-    }
-};
--- week03/week03-2-2.c
+++
@@ -1,6 +0,0 @@
-a=input()
-a=int(a)
-if a%2==1:
-	print("odd",end="")
-else:
-	print("even",end="")
